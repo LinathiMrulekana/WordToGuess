f1d4af418fff6562ec81fc313cb4774ff9ab08ad
LinathiMrulekana <102727009+LinathiMrulekana@users.noreply.github.com>
2022-05-18 09:52:33 +0200

    Add project files.

---
+++ GeussingGame.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WordToGuess
+{
+    public class GeussingGame
+    {
+        private string _guessWord;
+        private string _guessProgress;
+
+
+
+        public GeussingGame(string guessWord)
+        {
+            _guessWord = guessWord;
+            for (int index = 0; index < _guessWord.Length; index++)
+            {
+                _guessProgress += "*";
+            }
+        }
+        public string GetGeussProgress()
+        {
+            return _guessProgress;
+        }
+
+
+        public void AddGuess(char letter)
+        {
+            char[] guessProgressArray = _guessProgress.ToCharArray();
+
+            for (int index = 0; index < _guessWord.Length; index++)
+            {
+                if (_guessWord[index] == letter)
+                {
+                    guessProgressArray[index] = letter;
+                }
+            }
+            _guessProgress = new string( guessProgressArray);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace WordToGuess
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            GeussingGame game = new GeussingGame("Harry Potter");
+            Console.WriteLine(game.GetGeussProgress());
+            while (true)
+            {
+                string guess = Console.ReadLine();
+                game.AddGuess(guess[0]);
+
+                Console.WriteLine("Enter your next guess: ");
+                Console.WriteLine(game.GetGeussProgress());
+            }
+        }
+    }
+}
